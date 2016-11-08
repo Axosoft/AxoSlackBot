01413ad0c58799336f00c9ebb029d57ac20563e6
Brett Goldman <brettg@axosoft.com>
2016-11-08 09:12:31 -0700

    Add varible options to storeConfig

--- scripts/storeConfig.js
+++ scripts/storeConfig.js
@@ -1,6 +1,28 @@
 var fs = require('fs')
 
-console.log('writing config file...')
-if (process.env.AXO_slackbot_config) {
-  fs.writeFileSync('../config.json', process.env.AXO_slackbot_config)
+var config = {};
+
+//set defaults
+config.clientId = '82854487175.85597041971';
+config.port = 4114;
+config.axosoftClientId = "d4a4d398-2578-4cc7-a1cf-1b4a02330774"
+
+if (process.env.NODE_ENV == 'development') {
+config.redirectUri = 'devslackbot.axosoft.com:4114',
+config.mongoUri = "mongodb://devslackbot.axosoft.com:27017/slackData"
 }
+
+if (process.env.NODE_ENV == 'production') {
+config.redirectUri = 'slackbot.axosoft.com:4114',
+config.mongoUri = "mongodb://slackbot.axosoft.com:27017/slackData"
+}
+
+if (process.env.AXO_slackbot_secret && process.env.AXO_slackbot_axosoft_secret) {
+console.log('updating config file...')
+    config.clientSecret = process.env.AXO_slackbot_secret;
+    config.axosoftClientSecret = process.env.AXO_slackbot_axosoft_secret;
+}
+
+// write to file
+console.log(config);
+fs.writeFileSync('./config.json', JSON.stringify(config));
