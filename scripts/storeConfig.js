093619fdb324ba4dbb5fa0b2e99bbec926d37641
Brett Goldman <brettg@axosoft.com>
2016-11-16 08:08:14 -0700

    update redirect for localhost

--- scripts/storeConfig.js
+++ scripts/storeConfig.js
@@ -13,7 +13,7 @@ if (prefix == 'production') {
     prefix = '';
 }
 
-config.redirectUri = prefix + 'slackbot.axosoft.com:3004';
+config.redirectUri = 'http://localhost:3004/oauth';
 config.mongoUri = 'mongodb://localhost:27017/slackData'
 
 if (process.env.AXO_slackbot_secret && process.env.AXO_slackbot_axosoft_secret) {
