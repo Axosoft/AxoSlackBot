b238618eb112316c78b382bc827cc0f858c41813
Brett Goldman <brettg@axosoft.com>
2016-12-15 10:52:33 -0700

    update storeConfig for multiple clientIds

--- scripts/storeConfig.js
+++ scripts/storeConfig.js
@@ -3,14 +3,21 @@ var fs = require('fs')
 var config = {};
 
 //set defaults
-config.clientId = '2574228432.104777884963';
 config.port = 3004;
 config.axosoftClientId = '4b62de06-1f39-41f7-ae38-37ec5262478a'
 
 var prefix = process.env.BUILD_LIFECYCLE;
 
-if (prefix == 'production') {
-    prefix = '';
+if (process.env.BUILD_LIFECYCLE == 'dev') {
+    config.clientId = '2574228432.116076023584';
+}
+else if (process.env.BUILD_LIFECYCLE == 'staging') {
+    config.clientId = '2574228432.117458216726';
+}
+
+else if (process.env.BUILD_LIFECYCLE == 'production') {
+    config.clientId = '2574228432.104777884963';
+    prefix = '';    
 }
 
 config.baseUri = 'https://'+ prefix + 'slackbot.axosoft.com';
@@ -23,6 +30,4 @@ console.log('updating config file...')
 }
 
 // write to file
-// Don't want to write secrets to the log file...
-//console.log(config);
 fs.writeFileSync('./config.json', JSON.stringify(config));
